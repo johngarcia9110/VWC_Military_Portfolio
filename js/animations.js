5fd90080a5968334c3a83b9f7c5f9e757fdfb3cb
John Garcia <johngarcia9110@gmail.com>
2016-08-15 19:54:49 +0000

    added section animations + Easter egg on click in logo

---
+++ js/animations.js
@@ -0,0 +1,39 @@
+// $(window).scroll(function() {    
+//     var scroll = $(window).scrollTop();    
+//     if (scroll <= 500) {
+//         $(".text-pop").addClass("fadeIn");
+//         $("#myCarousel").addClass("fadeIn");
+//     }
+//     if (scroll <= 800){
+//         $(".text-pop").addClass("fadeIn");
+//         $("#myCarouse12").addClass("fadeIn");
+//     }
+// });
+function isVisible($el) {
+  var winTop = $(window).scrollTop();
+  var winBottom = winTop + $(window).height();
+  var elTop = $el.offset().top;
+  var elBottom = elTop + $el.height();
+  return ((elBottom<= winBottom) && (elTop >= winTop));
+}
+
+$(window).scroll(function () {
+    
+    if (isVisible($('#row1')) == true ) {
+        $('#row1').addClass('fadeIn');
+    }
+      
+    if (isVisible($('#row2')) == true ) {
+      $('#row2').addClass('fadeIn');
+    }
+    if (isVisible($('#row3')) == true ) {
+      $('#row3').addClass('fadeIn');
+    }
+   
+
+
+});
+
+$('.makeItFly').on('click', function(){
+  $(this).addClass('zoomOutRight');
+});
